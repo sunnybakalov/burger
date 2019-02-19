cad73af6e595bd7f734b0ac107ff379f54434d6b
Stan Bakalov <sunny.bakalov@gmail.com>
2019-02-18 21:09:12 -0700

    submitting to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,17 @@
 var mysql = require("mysql");
 require('dotenv').config()
 
-
-var connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: process.env.PASSWORD,
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: process.env.PASSWORD,
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
