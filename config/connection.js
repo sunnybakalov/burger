e9a21613cb4e47e4ce6ec42f0e63dc98ec152a0c
Stan Bakalov <sunny.bakalov@gmail.com>
2019-02-20 18:12:33 -0700

    aoidfud

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
     host: "127.0.0.1",
     port: 3306,
     user: "root",
-    password: process.env.PASSWORD,
+    password: "root",
     database: "burgers_db"
   });
 }
